ace2fa718e1acf454118ca1082b349d8dfc099ff
Naser <naser.r@gmail.com>
2023-08-20 18:43:00 +0330

    Add Console testing project
    Added a new Console testing project to the solution. 'CMD_Test' project was created to allow running different commands on different OS shells and view their outputs. This will facilitate command output testing in Linux and Windows environments. New Dockerfile is also added to create an environment for the test.

---
+++ src/Agent/Console/CMD_Test/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CMD_Test;
+
+Console.WriteLine("OS: " + Environment.OSVersion);
+
+var process = new RunCommand();
+
+var inputStream = process.RunProcess("cmd", "", Console.WriteLine, Console.WriteLine);
+//var inputStream = process.RunProcess("powershell", "", Console.WriteLine, s => Console.WriteLine(s));
+//var inputStream = process.RunProcess("bash", "", Console.WriteLine, s => Console.WriteLine(s));
+
+
+//Console.Write("Command> ");
+var command = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(command) || process.IsExited)
+{
+    return;
+}
+
+do
+{
+    //RunCommand.RunProcess("ping", "8.8.8.8", Console.WriteLine, s => Console.WriteLine("Err->" + s));
+
+    inputStream.Invoke(command!);
+    if (process.IsExited)
+    {
+        break;
+    }
+
+    //Console.Write("Command> ");
+    command = Console.ReadLine();
+} while (!string.IsNullOrWhiteSpace(command) || process.IsExited);
+
+process.Dispose();
+
+Console.WriteLine("Finished.");
