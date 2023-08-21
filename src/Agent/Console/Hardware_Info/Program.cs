b08061e3482f8b39b083cb9fc52d7ff3bbfccbaa
Naser <naser.r@gmail.com>
2023-08-21 16:28:15 +0330

    Add Hardware_Info project to solution
    This commit incorporates the addition of the "Hardware_Info" project into the Virtualization Manager solution. Accomplished by modifying the solution file and including the "Hardware_Info" project. A new directory, "Hardware_Info", is introduced in the "Agent/Console" directory with related Dockerfile, Program.cs file and project file, Hardware_Info.csproj. This new project aims to provide a programmatic interface to retrieve system information related to hardware. The Dockerfile outlines steps to build, publish the application using .NET 7.0 runtime and SDK. Program.cs prints detailed hardware-related information to the console, making use of the "Hardware.Info" NuGet package (version 11.1.1

---
+++ src/Agent/Console/Hardware_Info/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using Hardware.Info;
+
+Console.WriteLine(RuntimeInformation.OSDescription);
+Console.WriteLine(RuntimeInformation.FrameworkDescription);
+Console.WriteLine(RuntimeInformation.RuntimeIdentifier);
+
+// var startCpuUsage = Process.GetProcesses().Sum(a => a.TotalProcessorTime.TotalMilliseconds);
+//
+// Console.WriteLine(startCpuUsage);
+
+
+Console.WriteLine("Hello, World!");
+IHardwareInfo hardwareInfo = new HardwareInfo();
+hardwareInfo.RefreshAll();
+
+Console.WriteLine(hardwareInfo.OperatingSystem);
+
+Console.WriteLine(hardwareInfo.MemoryStatus);
+
+foreach (var hardware in hardwareInfo.BatteryList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.BiosList)
+    Console.WriteLine(hardware);
+
+foreach (var cpu in hardwareInfo.CpuList)
+{
+    Console.WriteLine(cpu);
+
+    foreach (var cpuCore in cpu.CpuCoreList)
+        Console.WriteLine(cpuCore);
+}
+
+//Console.ReadLine();
+
+foreach (var drive in hardwareInfo.DriveList)
+{
+    Console.WriteLine(drive);
+
+    foreach (var partition in drive.PartitionList)
+    {
+        Console.WriteLine(partition);
+
+        foreach (var volume in partition.VolumeList)
+            Console.WriteLine(volume);
+    }
+}
+
+//Console.ReadLine();
+
+foreach (var hardware in hardwareInfo.KeyboardList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.MemoryList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.MonitorList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.MotherboardList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.MouseList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.NetworkAdapterList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.PrinterList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.SoundDeviceList)
+    Console.WriteLine(hardware);
+
+foreach (var hardware in hardwareInfo.VideoControllerList)
+    Console.WriteLine(hardware);
+
+//Console.ReadLine();
+
+foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Ethernet, OperationalStatus.Up))
+    Console.WriteLine(address);
+
+Console.WriteLine();
+
+foreach (var address in HardwareInfo.GetLocalIPv4Addresses(NetworkInterfaceType.Wireless80211))
+    Console.WriteLine(address);
+
+Console.WriteLine();
+
+foreach (var address in HardwareInfo.GetLocalIPv4Addresses(OperationalStatus.Up))
+    Console.WriteLine(address);
+
+Console.WriteLine();
+
+foreach (var address in HardwareInfo.GetLocalIPv4Addresses())
+    Console.WriteLine(address);
+
+//Console.ReadLine();
