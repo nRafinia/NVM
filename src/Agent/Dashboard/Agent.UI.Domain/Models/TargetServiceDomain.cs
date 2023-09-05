a499c19f72fc41d2b4fa9f3cd7df914299c6e48e
Naser <naser.r@gmail.com>
2023-09-05 16:19:09 +0330

    Add theme

--- src/Agent/Dashboard/Agent.UI.Domain/Models/TargetServiceDomain.cs
+++ src/Agent/Dashboard/Agent.UI.Domain/Models/TargetServiceDomain.cs
@@ -2,7 +2,9 @@ namespace Agent.UI.Domain.Models;
 
 public class TargetServiceDomain
 {
-    public string Domain{ get; private set; }
+    public string Domain { get; private set; } = string.Empty;
+
+    public bool IsReady => !string.IsNullOrWhiteSpace(Domain);
 
     public void SetDomain(string domain)
     {
