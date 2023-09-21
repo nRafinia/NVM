cc1b5b89ca586d2ff5f9e2ff9c3ffc0d64ae72be
Naser <naser.r@gmail.com>
2023-09-21 19:30:13 +0330

    "Expand file type icons and implement drive selection"
    This commit expands the MIME type -> file icon mapping dictionary to cover a wider range of common file formats. Detailed MIME types for multiple categories, including image, audio, video, document types and archive files have been added for more accurate icon representation increasing user ability to recognize file types quickly.
    
    In addition, the filesystem navigation UI in the dashboard has been extended to allow for the selection of drives on systems where this is applicable. This offers greater flexibility and convenience to users, by enabling navigation across different logical drives without leaving the dashboard interface.

--- src/Agent/Dashboard/Agent.UI/Helpers/FileTypeIcon.cs
+++ src/Agent/Dashboard/Agent.UI/Helpers/FileTypeIcon.cs
@@ -17,9 +17,37 @@ public static class FileTypeIcon
         var iconMapping = new Dictionary<string, string>()
         {
             {"image", "fa-file-image"},
+            {"image/png", "fa-file-image"},
+            {"image/x-icon", "fa-file-image"},
+            {"image/jpeg", "fa-file-image"},
+            {"image/bmp", "fa-file-image"},
+            {"image/avif", "fa-file-image"},
+            {"image/gif", "fa-file-image"},
+            {"image/tiff", "fa-file-image"},
+            {"image/webp", "fa-file-image"},
+            
             {"audio", "fa-file-audio"},
+            {"audio/aac", "fa-file-audio"},
+            {"application/x-cdf", "fa-file-audio"},
+            {"audio/midi", "fa-file-audio"},
+            {"audio/x-midi", "fa-file-audio"},
+            {"audio/mpeg", "fa-file-audio"},
+            {"audio/ogg", "fa-file-audio"},
+            {"audio/opus", "fa-file-audio"},
+            {"audio/wav", "fa-file-audio"},
+            {"audio/webm", "fa-file-audio"},
+            
             {"video", "fa-file-video"},
+            {"video/x-msvideo", "fa-file-video"},
+            {"video/mp4", "fa-file-video"},
+            {"video/mpeg", "fa-file-video"},
+            {"video/ogg", "fa-file-video"},
+            {"video/mp2t", "fa-file-video"},
+            {"video/webm", "fa-file-video"},
+            
             {"application/pdf", "fa-file-pdf"},
+            {"application/java-archive", "fa-java"},
+            
             {"application/msword", "fa-file-word"},
             {"application/vnd.ms-word", "fa-file-word"},
             {"application/vnd.oasis.opendocument.text", "fa-file-word"},
@@ -30,11 +58,30 @@ public static class FileTypeIcon
             {"application/vnd.ms-powerpoint", "fa-file-powerpoint"},
             {"application/vnd.openxmlformats-officedocument.presentationml","fa-file-powerpoint"},
             {"application/vnd.oasis.opendocument.presentation", "fa-file-powerpoint"},
+            
             {"text/plain", "fa-file-text"},
             {"text/html", "fa-file-code"},
+            {"text/csv", "fa-file-csv"},
+            {"text/css", "fa-file-text"},
+            
             {"application/json", "fa-file-code"},
+            {"application/ld+json", "fa-file-code"},
+            {"application/x-sh", "fa-file-code"},
+            {"application/xhtml+xml", "fa-file-code"},
+            {"application/html", "fa-file-code"},
+            {"application/x-httpd-php", "fa-file-code"},
+            {"text/javascript", "fa-file-code"},
+            {"application/x-javascript", "fa-file-code"},
+            {"application/x-csh", "fa-file-code"},
+            
             {"application/gzip", "fa-file-archive"},
-            {"application/zip", "fa-file-archive"}
+            {"application/x-bzip", "fa-file-archive"},
+            {"application/x-bzip2", "fa-file-archive"},
+            {"application/x-freearc", "fa-file-archive"},
+            {"application/x-tar", "fa-file-archive"},
+            {"application/vnd.rar", "fa-file-archive"},
+            {"application/x-7z-compressed", "fa-file-archive"},
+            {"application/zip", "fa-file-zip"}
         };
 
         return iconMapping.ContainsKey(mimeType) ? iconMapping[mimeType] : "fa-file";
