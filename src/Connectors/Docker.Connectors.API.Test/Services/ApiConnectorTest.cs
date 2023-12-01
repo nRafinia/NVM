caf0db0f0f3e0765c8d8fe77422e3d45620753dc
Naser <naser.r@gmail.com>
2023-12-01 17:39:23 +0330

    Add ApiConnectorTest with UserSecrets configuration
    This commit adds a new test class 'ApiConnectorTest' within the Docker.Connectors.API.Test project, further enhancing the project's tests coverage. This class tests the ApiConnector class and its functionality using Moq. Here, the server IP is provided through UserSecrets to improve security and avoid hard coding credentials. Hence, 'Microsoft.Extensions.Configuration.UserSecrets' is added as a package reference and 'Microsoft.Extensions.SecretManager.Tools' as a DotNetCliToolReference.

---
+++ src/Connectors/Docker.Connectors.API.Test/Services/ApiConnectorTest.cs
@@ -0,0 +1,38 @@
+using Docker.Connectors.API.Authentications;
+using Docker.Connectors.API.Services;
+using Docker.DotNet;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Docker.Connectors.API.Test.Services;
+
+public class ApiConnectorTest
+{
+    private readonly IConfiguration _configuration;
+
+    public ApiConnectorTest()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<ApiConnectorTest>();
+
+        _configuration = builder.Build();
+    }
+    
+    [Fact]
+    public async Task GetImages_Success()
+    {
+        //arrange
+        var dockerClientConfiguration =
+            new DockerClientConfiguration(new Uri($"http://{_configuration["serverip"]}:2375"), new AnonymousCredentials());
+        var authenticator = new Mock<IApiAuthenticate>();
+        authenticator.Setup(a => a.GetCredentials())
+            .Returns(dockerClientConfiguration);
+
+        //act
+        var connector = new ApiConnector(authenticator.Object);
+        var images = await connector.GetImages();
+
+        //assert
+        Assert.NotEmpty(images);
+    }
+}
