d663ec887caa0e8d01118b28c2edb1452ef7f52c
Naser <naser.r@gmail.com>
2023-12-02 10:02:13 +0330

    Add Common.Test project and SizeConverter tests
    Common.Test project is added to the solution to allow unit testing of Common functionalities. The project includes Xunit testing framework and its related dependencies. Moreover, a SizeConverterTests.cs file is created to include relevant unit tests for SizeConverter methods. These tests aim to ensure proper conversion between byte sizes and their string representations. This is crucial for managing file or system size related tasks within the application.

---
+++ src/Shared/Common.Test/SizeConverterTests.cs
@@ -0,0 +1,40 @@
+namespace Common.Test;
+
+public class SizeConverterTests
+{
+    [Fact]
+    public void ConvertBytesToString_ReturnsCorrectString_ForPositiveBytes()
+    {
+        var result = SizeConverter.ConvertBytesToString(1500);
+        Assert.Equal("1.5 KB", result);
+    }
+
+    [Fact]
+    public void ConvertBytesToString_ReturnsCorrectString_ForNegativeBytes()
+    {
+        var result = SizeConverter.ConvertBytesToString(-1500);
+        Assert.Equal("-1.5 KB", result);
+    }
+
+    [Fact]
+    public void ConvertBytesToString_ReturnsCorrectString_ForZeroBytes()
+    {
+        var result = SizeConverter.ConvertBytesToString(0);
+        Assert.Equal("0.0 bytes", result);
+    }
+
+    [Fact]
+    public void ConvertStringToBytes_ReturnsCorrectBytes_ForValidString()
+    {
+        var result = SizeConverter.ConvertStringToBytes("1.5KB");
+        Assert.Equal(1536, result);
+    }
+
+    [Fact]
+    public void ConvertStringToBytes_ReturnsNegativeOne_ForInvalidString()
+    {
+        var result = SizeConverter.ConvertStringToBytes("invalid string");
+        Assert.Equal(-1, result);
+    }
+
+}
