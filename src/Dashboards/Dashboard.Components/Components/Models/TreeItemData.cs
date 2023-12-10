35ee298c07b3ece9e1eef34cd87c7d906499e3ba
Naser <naser.r@gmail.com>
2023-12-10 14:27:07 +0330

    Refactor tree view components and add icons
    Removed unnecessary 'Id' parameter from the TreeItemData class and added an 'Icon' parameter instead. The 'TreeViewItems' components was moved to a new library named 'Dashboard.Components', and an icon image tag was added to the component. Modified NavMenu to reflect these changes. The tree view item styles were also updated in the css file.

--- src/Dashboards/Dashboard.Components/Components/Models/TreeItemData.cs
+++ src/Dashboards/Dashboard.Components/Components/Models/TreeItemData.cs
@@ -1,26 +1,23 @@
-using System.Runtime.InteropServices;
-
 namespace Dashboard.Components.Models;
 
 public class TreeItemData<T>
 {
-    public string Id { get; init; }
-    public TreeItemData<T>? Parent { get; private set; }
     public string Text { get; private set; }
+    public TreeItemData<T>? Parent { get; private set; }
     public bool IsExpanded { get; private set; }
-
+    public string? Icon { get; set; }
     public T? Data { get; set; }
 
-    private List<TreeItemData<T>> _children= new();
+    private readonly List<TreeItemData<T>> _children= new();
     public IReadOnlyList<TreeItemData<T>> Children => _children.AsReadOnly();
     public bool HasChildren => _children.Any();
 
-    public TreeItemData(string id, string text, T? data, bool isExpanded = false)
+    public TreeItemData(string text, T? data, bool isExpanded = false, string? icon = null)
     {
-        Id = id;
         Text = text;
         Data = data;
         IsExpanded = isExpanded;
+        Icon = icon;
     }
 
     public void Expand()
