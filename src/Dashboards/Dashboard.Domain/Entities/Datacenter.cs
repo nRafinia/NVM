f0d07ca114e2965e0095e195556aa244480fe2b3
Naser <naser.r@gmail.com>
2023-12-14 20:49:22 +0330

    Add Datacenter, Cluster and Host entities
    Three new entity classes, Datacenter, Cluster, and Host, have been added to the Dashboard.Domain.Entities namespace. These classes define the ID, Name and optional Description fields, with private methods to update these attributes.

---
+++ src/Dashboards/Dashboard.Domain/Entities/Datacenter.cs
@@ -0,0 +1,13 @@
+namespace Dashboard.Domain.Entities;
+
+public class Datacenter(string id, string name, string? description = null) : Entity(id)
+{
+    public string Name { get; private set; } = Guard.Against.NullOrEmpty(name, nameof(name));
+    public string? Description { get; private set; } = description;
+
+    private void Update(string newName, string? newDescription = null)
+    {
+        Name = Guard.Against.NullOrEmpty(newName, nameof(newName));
+        Description = newDescription;
+    }
+}
