e4a5a719d5b76c2d37541710d507e1d4f65dffdb
Naser <naser.r@gmail.com>
2023-12-24 10:44:25 +0330

    Remove AddApiCredentials related classes and adjustments in csproj
    The AddApiCredentials related classes, specifically the AddApiCredentialBasic and AddApiCredentialNone, along with their validators have been deleted. Additionally, a reference to a non-existing folder in the csproj file has been removed.

--- src/Dashboards/Dashboard.Application/Credentials/AddApiCredentials/AddApiCredentialsBasic/AddApiCredentialBasic.cs
+++
@@ -1,7 +0,0 @@
-using Dashboard.Domain.Base.Commands;
-using Dashboard.Domain.Base.Results;
-
-namespace Dashboard.Application.Credentials.AddApiCredentials.AddApiCredentialsBasic;
-
-public record AddApiCredentialBasic(string Name, string UserName, string Password, string? Description = default)
-    : ICommand<Result>;
--- src/Dashboards/Dashboard.Application/Credentials/AddApiCredentials/AddApiCredentialsNone/AddApiCredentialNone.cs
+++
@@ -1,6 +0,0 @@
-using Dashboard.Domain.Base.Commands;
-using Dashboard.Domain.Base.Results;
-
-namespace Dashboard.Application.Credentials.AddApiCredentials.AddApiCredentialsNone;
-
-public record AddApiCredentialNone(string Name, string? Description = default) : ICommand<Result>;
