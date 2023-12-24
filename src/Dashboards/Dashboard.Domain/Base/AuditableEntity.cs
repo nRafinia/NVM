61a5eecdc24a8afa081e64a27b452bbb14e9036a
Naser <naser.r@gmail.com>
2023-12-24 19:44:57 +0330

    Change data type of author fields in AuditableEntity
    The `CreatedBy` and `LastUpdatedBy` fields data types were updated from string to `IdColumn`. This change allows the system to treat users who create or update entities more consistently and robustly.

--- src/Dashboards/Dashboard.Domain/Base/AuditableEntity.cs
+++ src/Dashboards/Dashboard.Domain/Base/AuditableEntity.cs
@@ -5,7 +5,7 @@ namespace Dashboard.Domain.Base;
 public class AuditableEntity(IdColumn id) : Entity(id)
 {
     public DateTime Created { get; set; }
-    public string? CreatedBy { get; set; }
+    public IdColumn? CreatedBy { get; set; }
     public DateTime? LastUpdated { get; set; }
-    public string? LastUpdatedBy { get; set; }
+    public IdColumn? LastUpdatedBy { get; set; }
 }
