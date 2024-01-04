6759fbee9d3272705773ad688e350289b7b16fb7
Naser <naser.r@gmail.com>
2024-01-04 17:30:29 +0330

    Add unit tests for UpdateCredential handlers
    This commit introduces unit tests for UpdateCredentialBasicHandler and UpdateCredentialNoneHandler in the Dashboard application. The tests verify scenarios such as success, failure due to missing credential, error logging, and credential type conversion.

---
+++ src/Dashboards/Dashboard.Application.Test/Credentials/Commands/UpdateCredentials/UpdateCredentialsNone/UpdateCredentialNoneHandlerTest.cs
@@ -0,0 +1,61 @@
+using Dashboard.Application.Credentials.Commands.UpdateCredentials.UpdateCredentialsNone;
+using JetBrains.Annotations;
+using Moq;
+using Dashboard.Domain.Abstractions.Repositories;
+using Dashboard.Domain.Entities;
+using Dashboard.Domain.Enums;
+using Dashboard.Domain.ValueObjects;
+using Microsoft.Extensions.Logging;
+
+namespace Dashboard.Application.Test.Credentials.Commands.UpdateCredentials.UpdateCredentialsNone;
+
+public class UpdateCredentialNoneHandlerTests
+{
+    private readonly Mock<ICredentialRepository> _repository;
+    private readonly UpdateCredentialNoneHandler _handler;
+    
+    public UpdateCredentialNoneHandlerTests()
+    {
+        _repository = new Mock<ICredentialRepository>();
+        var logger = new Mock<ILogger<UpdateCredentialNoneHandler>>();
+        _handler = new UpdateCredentialNoneHandler(_repository.Object, logger.Object);
+    }
+    
+    [Fact]
+    public async Task Handle_Success()
+    {
+        // Arrange
+        var stubCredential = Credential.None("UserName");
+        var request = new UpdateCredentialNone(stubCredential.Id, "testName", "testDescription");
+        
+        _repository
+            .Setup(r => r.GetAsync(It.IsAny<IdColumn>()))
+            .ReturnsAsync(stubCredential);
+        
+        // Act
+        var result = await _handler.Handle(request, default);
+        
+        //Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal("testName", stubCredential.Name);
+    }    
+    
+    [Fact]
+    public async Task Handle_ConvertBasicToNone_Success()
+    {
+        // Arrange
+        var basicCredential = Credential.Basic("UserName","UserName", "Password");
+        var request = new UpdateCredentialNone(basicCredential.Id, "testName");
+        
+        _repository
+            .Setup(r => r.GetAsync(It.IsAny<IdColumn>()))
+            .ReturnsAsync(basicCredential);
+        
+        // Act
+        var result = await _handler.Handle(request, default);
+        
+        //Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(CredentialType.None, basicCredential.CredentialType);
+    }
+}
