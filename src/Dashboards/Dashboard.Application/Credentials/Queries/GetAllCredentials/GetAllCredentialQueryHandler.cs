7b8fa8d0f268c000e0ee3c6828d5b12e5ca6f2d7
Naser <naser.r@gmail.com>
2024-01-04 18:38:14 +0330

    Add GetAllCredentials query and handler
    This commit introduces a new query and its corresponding handler to fetch all credentials. It includes error handling logic in the query handler and unit tests to verify the successful and failed scenarios.

---
+++ src/Dashboards/Dashboard.Application/Credentials/Queries/GetAllCredentials/GetAllCredentialQueryHandler.cs
@@ -0,0 +1,25 @@
+using Dashboard.Domain.Base.Queries;
+
+namespace Dashboard.Application.Credentials.Queries.GetAllCredentials;
+
+public class GetAllCredentialQueryHandler(
+    ICredentialRepository repository,
+    ILogger<GetAllCredentialQueryHandler> logger)
+    : IQueryHandler<GetAllCredentialQuery, IReadOnlyList<Credential>>
+{
+    public async Task<Result<IReadOnlyList<Credential>?>> Handle(GetAllCredentialQuery request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await repository.GetAllAsync();
+            return Result.Success<IReadOnlyList<Credential>?>(result);
+        }
+        catch (Exception e)
+        {
+            // ReSharper disable once LogMessageIsSentenceProblem
+            logger.LogError(e, "Error in add none credential.");
+            return e.ToResult<IReadOnlyList<Credential>>();
+        }
+    }
+}
