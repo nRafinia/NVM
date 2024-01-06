cce4fea6849e149a68e68271cec41a55708651cd
Naser <naser.r@gmail.com>
2024-01-06 16:14:23 +0330

    Refactor EncryptDataModel from struct to class
    The EncryptDataModel has been changed from a record struct to a class in the Vault utility. In addition, JsonPropertyNames have been added to the Ciphertext, Nonce, and Tag properties to ensure proper serialization.

--- src/Utility/Vault/Models/EncryptDataModel.cs
+++ src/Utility/Vault/Models/EncryptDataModel.cs
@@ -1,3 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace Vault.Models;
 
-internal record struct EncryptDataModel(byte[] Ciphertext, byte[] Nonce, byte[] Tag);
+internal class EncryptDataModel(
+    byte[] cipherText,
+    byte[] nonce,
+    byte[] tag)
+{
+    [JsonPropertyName("c")]
+    public byte[] Ciphertext { get; set; } = cipherText;
+    
+    [JsonPropertyName("n")]
+    public byte[] Nonce { get; set; } = nonce;
+    
+    [JsonPropertyName("t")]
+    public byte[] Tag { get; set; } = tag;
+
+}
