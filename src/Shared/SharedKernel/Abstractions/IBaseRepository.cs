1d1184e21dc33fc9f4bd2e9eda13c7b766cd4193
Naser <naser.r@gmail.com>
2024-01-09 17:00:15 +0330

    Update default parameters and methods in IBaseRepository
    This update makes CancellationToken parameters in IBaseRepository methods optional by setting default values. Also, it refactors 'FindByField' methods to more general 'GetAllAsync' with predicate option, enhancing flexibility for entity retrieval.

--- src/Shared/SharedKernel/Abstractions/IBaseRepository.cs
+++ src/Shared/SharedKernel/Abstractions/IBaseRepository.cs
@@ -1,19 +1,19 @@
-using System.Linq.Expressions;
-
 namespace SharedKernel.Abstractions;
 
 public interface IBaseRepository<TEntity>
     where TEntity : Entity
 {
-    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken);
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
     Task DeleteAsync(IdColumn id, CancellationToken cancellationToken = default);
-    ValueTask<TEntity?> GetAsync(IdColumn id, CancellationToken cancellationToken);
 
-    Task<List<TEntity>> FindByField(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken cancellationToken);
+    ValueTask<TEntity?> GetAsync(IdColumn id, CancellationToken cancellationToken = default);
+    ValueTask<TEntity?> GetAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default);
+    Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
 
-    Task<List<TEntity>> FindByField(Expression<Func<TEntity, bool>> predicate, int index, int size,
-        CancellationToken cancellationToken);
+    Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default);
+
+    Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate, int index, int size,
+        CancellationToken cancellationToken = default);
 }
