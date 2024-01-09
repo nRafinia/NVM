5284435edb0e2df5531c9126b139c287543e6227
Naser <naser.r@gmail.com>
2024-01-09 21:58:53 +0330

    Update method parameters in IBaseRepository
    This commit updates method parameters in the IBaseRepository interface. Func predicates are changed to Expressions, which allows more complex querying and improved compile-time safety while calling these methods.

--- src/Shared/SharedKernel/Abstractions/IBaseRepository.cs
+++ src/Shared/SharedKernel/Abstractions/IBaseRepository.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace SharedKernel.Abstractions;
 
 public interface IBaseRepository<TEntity>
@@ -9,11 +11,14 @@ public interface IBaseRepository<TEntity>
     Task DeleteAsync(IdColumn id, CancellationToken cancellationToken = default);
 
     ValueTask<TEntity?> GetAsync(IdColumn id, CancellationToken cancellationToken = default);
-    ValueTask<TEntity?> GetAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default);
+
+    ValueTask<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
     Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
 
-    Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken = default);
+    Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
 
-    Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate, int index, int size,
+    Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, int index, int size,
         CancellationToken cancellationToken = default);
 }
