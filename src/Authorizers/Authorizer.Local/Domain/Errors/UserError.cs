6370b18c4e812327e2d59e77ebc7bd9f2a6ff145
Naser <naser.r@gmail.com>
2024-01-10 20:39:26 +0330

    Change UserError class visibility to internal
    The UserError class in the Authorizer.Local.Domain.Errors namespace has been modified to have internal visibility. This alteration provides more control and safety by limiting the accessibility of the UserError class within the assembly.

--- src/Authorizers/Authorizer.Local/Domain/Errors/UserError.cs
+++ src/Authorizers/Authorizer.Local/Domain/Errors/UserError.cs
@@ -2,7 +2,7 @@ using SharedKernel.Base;
 
 namespace Authorizer.Local.Domain.Errors;
 
-public static class UserError
+internal static class UserError
 {
     public static readonly Error UserIsInactive = new("user.inactive", "User is inactive.");
     public static readonly Error PasswordIsInvalid = new("user.password.invalid", "Password is invalid.");
