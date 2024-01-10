8026529085499568be3bc07f7eae8d597478eb40
Naser <naser.r@gmail.com>
2024-01-10 21:10:02 +0330

    Refactor Credential page for better structure
    Moved LoadData() and Delete() methods from Credential.razor to Credential.razor.cs to improve separation of logic and presentation. Also, made the GetCredentialLabelType() function static as it does not depend on any instance variables. This change improves the code maintainability and readability which will support future development.

--- src/Dashboards/Dashboard/Pages/Dashboards/Credentials/Credential.razor.cs
+++ src/Dashboards/Dashboard/Pages/Dashboards/Credentials/Credential.razor.cs
@@ -1,4 +1,10 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Dashboard.Application.Credentials.Commands.CommitCredentialsChanges;
+using Dashboard.Application.Credentials.Commands.DeleteCredentials;
+using Dashboard.Application.Credentials.Queries.GetAllCredentials;
+using Dashboard.Application.Credentials.Queries.GetCredentialByName;
 using Dashboard.Const;
+using Dashboard.Domain.ValueObjects;
 
 namespace Dashboard.Pages.Dashboards.Credentials;
 
@@ -6,4 +12,53 @@ public partial class Credential : DashboardPage
 {
     public readonly string PageTitle = "Credential";
     public override Guid Id => DashboardIds.Credential;
+    
+    private async Task LoadData()
+    {
+        var credentialsResponse = string.IsNullOrWhiteSpace(SearchText)
+            ? await Mediator.Send(new GetAllCredentialQuery())
+            : await Mediator.Send(new GetCredentialByNameQuery(SearchText!));
+        if (credentialsResponse.IsFailure)
+        {
+            await Swal.FireAsync("Error", credentialsResponse.Error!.Message, SweetAlertIcon.Error);
+            return;
+        }
+
+        _credentials = credentialsResponse.Value!.ToList();
+        StateHasChanged();
+    }
+    
+    private async Task Delete(IdColumn id)
+    {
+        var credential = _credentials.FirstOrDefault(x => x.Id == id);
+        if (credential is null)
+        {
+            return;
+        }
+
+        var deleteConfirmation = await Swal.FireAsync(new SweetAlertOptions("Delete")
+        {
+            Text = $"Are you sure for delete '{credential.Name}' credential?",
+            Icon = SweetAlertIcon.Warning,
+            ShowCancelButton = true,
+            ConfirmButtonText = "Yes, delete it!",
+            CancelButtonText = "No, keep it"
+        });
+
+        if (deleteConfirmation.IsDismissed)
+        {
+            return;
+        }
+
+        var deleteResponse = await Mediator.Send(new DeleteCredentialCommand(id));
+        if (deleteResponse.IsFailure)
+        {
+            await Swal.FireAsync("Error", deleteResponse.Error!.Message, SweetAlertIcon.Error);
+            return;
+        }
+
+        await Mediator.Send(new CommitCredentialChanges());
+
+        await LoadData();
+    }
 }
