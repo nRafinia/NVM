898dcac355755fe3712a50416371749bbdd6e710
Naser <naser.r@gmail.com>
2024-01-10 20:27:06 +0330

    Add username existence check in AddUserCommandHandler
    A check is added to ensure that the username does not already exist before a new user is created. This safeguards against duplicate usernames in the system, providing a fail-safe mechanism and improves the robustness of user creation process.

--- src/Authorizers/Authorizer.Local/Application/Users/Commands/AddUser/AddUserCommandHandler.cs
+++ src/Authorizers/Authorizer.Local/Application/Users/Commands/AddUser/AddUserCommandHandler.cs
@@ -1,9 +1,11 @@
+using Authorizer.Common.Models;
 using Authorizer.Local.Domain.Entities;
 using Authorizer.Local.Persistence.Repositories;
 using Microsoft.Extensions.Logging;
 using SharedKernel.Abstractions;
 using SharedKernel.Base.Commands;
 using SharedKernel.Base.Results;
+using SharedKernel.Errors;
 using SharedKernel.Extensions;
 
 namespace Authorizer.Local.Application.Users.Commands.AddUser;
@@ -18,6 +20,12 @@ public class AddUserCommandHandler(
     {
         try
         {
+            var existUser = await repository.IsExistUserNameAsync(request.UserName, cancellationToken);
+            if (existUser)
+            {
+                return Result.Failure<UserInfo>(SharedErrors.Duplicate("UserName already exists."));
+            }
+            
             var user = new User(request.UserName, request.Password, request.DisplayName);
             await repository.AddAsync(user, cancellationToken);
             await unitOfWork.CommitAsync(cancellationToken);
