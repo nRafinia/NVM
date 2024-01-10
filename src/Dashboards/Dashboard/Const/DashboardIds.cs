79df244667b528ee9da7cf9b6493c71e4f3d6413
Naser <naser.r@gmail.com>
2024-01-10 21:14:56 +0330

    Add User page to Dashboard
    User page has been added to the dashboard for better user management. A unique User ID is also added, for accurate identification and data management of users. An 'if' condition was included in the Index.razor file to render the User component when the current ID matches the User ID.

--- src/Dashboards/Dashboard/Const/DashboardIds.cs
+++ src/Dashboards/Dashboard/Const/DashboardIds.cs
@@ -4,4 +4,5 @@ public static class DashboardIds
 {
     public static Guid Main = Guid.Parse("f9639cd5-731a-4858-b93d-1117ae1422f1");
     public static Guid Credential = Guid.Parse("1b249242-5ba8-4c1e-9eee-6d48bbc5eab1");
+    public static Guid User = Guid.Parse("f1b6a9c8-c893-46f1-9a79-06c633ad5e28");
 }
