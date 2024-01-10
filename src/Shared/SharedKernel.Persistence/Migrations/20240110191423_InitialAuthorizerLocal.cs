0c4f9af0545581368c4aa1e08966c27b5f18f300
Naser <naser.r@gmail.com>
2024-01-10 22:56:39 +0330

    Add EF Core Design package and LDAP project reference
    The Dashboard project file has been updated with a new package reference to the EF Core Design package and a project reference to the Authorizer.LDAP. Several new migration files were also added under the SharedKernel.Persistence project. Additionally, a new connection string for the application was included in the Dashboard's appsettings.json file.

---
+++ src/Shared/SharedKernel.Persistence/Migrations/20240110191423_InitialAuthorizerLocal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedKernel.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialAuthorizerLocal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    LastUpdated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastUpdatedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
