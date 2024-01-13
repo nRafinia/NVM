083b00ff4a971c0c18fe406311110b77f3207262
Naser <naser.r@gmail.com>
2024-01-13 21:02:36 +0330

    Update entity model and create initial migration
    This commit introduces changes to the ApplicationDbContextModelSnapshot by updating the entity model, changing some property types to Guid and adjusting relationships. This required the creation of an initial migration script to align the database with the updated entity model rule. The update in the entity model will open the door to more robust and clear data handling within the application.

--- src/Shared/SharedKernel.Persistence/Migrations/20240113170828_Initial.Designer.cs
+++ src/Shared/SharedKernel.Persistence/Migrations/20240113170828_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SharedKernel.Persistence;
 
@@ -11,9 +12,11 @@ using SharedKernel.Persistence;
 namespace SharedKernel.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240113170828_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
---
+++ src/Shared/SharedKernel.Persistence/Migrations/20240113170828_Initial.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharedKernel.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LDAPs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Port = table.Column<int>(type: "INTEGER", nullable: false),
+                    UseSecure = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HostName = table.Column<string>(type: "TEXT", nullable: false),
+                    CredentialId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BaseDn = table.Column<string>(type: "TEXT", nullable: false),
+                    FilterQuery = table.Column<string>(type: "TEXT", nullable: false),
+                    Scope = table.Column<int>(type: "INTEGER", nullable: false),
+                    AuthenticationType = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProtocolVersion = table.Column<int>(type: "INTEGER", nullable: false),
+                    Attributes_DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    Attributes_UniqueId = table.Column<string>(type: "TEXT", nullable: false),
+                    Attributes_UserName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LDAPs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: true),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    AuthorizerType = table.Column<int>(type: "INTEGER", nullable: false),
+                    LdapId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "TEXT", nullable: true),
+                    LastUpdated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    LastUpdatedBy = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_LDAPs_LdapId",
+                        column: x => x.LdapId,
+                        principalTable: "LDAPs",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LDAPs_Name",
+                table: "LDAPs",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_LdapId",
+                table: "Users",
+                column: "LdapId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "LDAPs");
+        }
+    }
+}
