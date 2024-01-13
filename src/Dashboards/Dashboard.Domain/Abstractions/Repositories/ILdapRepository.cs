e6dd0c026ce0e4b166a5b9d328dc3fea440ccf3f
Naser <naser.r@gmail.com>
2024-01-13 12:26:24 +0330

    Add ILdapRepository interface
    The commit includes the addition of a new ILdapRepository interface under Dashboard.Domain.Abstractions.Repositories. This provides basic repository functions specific to LDAP entities, leveraging the shared kernel abstractions.

---
+++ src/Dashboards/Dashboard.Domain/Abstractions/Repositories/ILdapRepository.cs
@@ -0,0 +1,8 @@
+using Dashboard.Domain.Entities.LDAPs;
+using SharedKernel.Abstractions;
+
+namespace Dashboard.Domain.Abstractions.Repositories;
+
+public interface ILdapRepository : IBaseRepository<LDAP>
+{
+}
