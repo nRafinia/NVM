edd9e20ff395097dedfc6d2617e2b178e5d6272f
Naser <naser.r@gmail.com>
2024-01-13 12:22:31 +0330

    Add Dashboard.Persistence project and LDAP entity
    A new project, Dashboard.Persistence, has been added to the solution. The project includes an empty ConfigureServices class. Additionally, the LDAP entity has been added to the SharedKernel.Persistence project with a corresponding migration for the database. Changes have been made to the solution file to include the newly added projects.

---
+++ src/Dashboards/Dashboard.Persistence/ConfigureServices.cs
@@ -0,0 +1,15 @@
+using Mapster;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel.Shared;
+
+namespace Dashboard.Persistence;
+
+public class ConfigureServices : IConfigureService
+{
+    public void AddServices(IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+
+}
