455b24569489e79befdc2c660f3eadef4c538a0b
Naser <naser.r@gmail.com>
2024-01-14 15:07:33 +0330

    Add TypeAdapterConfig for LDAP and User entities
    This update adds two new Mapster TypeAdapterConfig to the ConfigureServices. It configures mapping for both LDAP to LdapConfiguration and User to UserSession. These mappings will be used to handle transformations between these two sets of entities within the application.

--- src/Dashboards/Dashboard.Application/ConfigureServices.cs
+++ src/Dashboards/Dashboard.Application/ConfigureServices.cs
@@ -1,11 +1,15 @@
+using Authorizer.Ldap.Models;
 using Dashboard.Application.Behaviors;
 using Dashboard.Application.Credentials;
 using Dashboard.Application.Users;
+using Dashboard.Domain.Entities.LDAPs;
+using Mapster;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SharedKernel.Abstractions;
 using SharedKernel.Shared;
+using LdapAttribute = Authorizer.Ldap.Models.LdapAttribute;
 
 namespace Dashboard.Application;
 
@@ -21,5 +25,29 @@ public class ConfigureServices : IConfigureService
 
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<ICredentialService, CredentialService>();
+
+        ConfigureMapping();
+    }
+
+    private static void ConfigureMapping()
+    {
+        TypeAdapterConfig<LDAP, LdapConfiguration>
+            .NewConfig()
+            .MapWith(ldap => new LdapConfiguration(ldap.HostName, ldap.CredentialId, ldap.BaseDn)
+            {
+                Port = ldap.Port,
+                UseSecure = ldap.UseSecure,
+                FilterQuery = ldap.FilterQuery,
+                Attributes = new LdapAttribute()
+                {
+                    DisplayName = ldap.Attributes.DisplayName,
+                    UserName = ldap.Attributes.UserName,
+                    UniqueId = ldap.Attributes.UniqueId
+                },
+                Scope = (System.DirectoryServices.Protocols.SearchScope)ldap.Scope,
+                AuthenticationType = (System.DirectoryServices.Protocols.AuthType)ldap.AuthenticationType,
+                ProtocolVersion = ldap.ProtocolVersion
+            });
+        
     }
 }
--- src/Dashboards/Dashboard/ConfigureServices.cs
+++ src/Dashboards/Dashboard/ConfigureServices.cs
@@ -1,6 +1,8 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Dashboard.Domain.Entities.Users;
 using Dashboard.Providers;
 using Dashboard.Services;
+using Mapster;
 using Microsoft.AspNetCore.Components.Authorization;
 using SharedKernel.Abstractions;
 using SharedKernel.Shared;
@@ -20,5 +22,15 @@ public class ConfigureServices : IConfigureService
         services.AddScoped<AuthenticationStateProvider, DashboardAuthentication>();
         services.AddScoped<IMainTreeViewService, MainTreeViewService>();
         services.AddSingleton<ICurrentUser, CurrentUserProvider>();
+        
+        ConfigureMapping();
+    }
+
+    private static void ConfigureMapping()
+    {
+        TypeAdapterConfig<User, UserSession>
+            .NewConfig()
+            .Map(d => d.UserName, s => s.UserName)
+            .Map(d => d.UserId, s => s.Id);
     }
 }
